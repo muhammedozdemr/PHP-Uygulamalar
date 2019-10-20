66cbd5490acf6e527df0f99fa5885a1fb8c02422
Muhammed <49456988+muhammedozdemr@users.noreply.github.com>
2019-10-20 15:46:42 +0300

    Update carpmaislemi.php

--- carpmaislemi.php
+++ carpmaislemi.php
@@ -5,7 +5,6 @@
   <form action="" method="get">
   1.Sayıyı Giriniz: <input type="text" name="sayi1"><br><br>
   2.Sayıyı Giriniz: <input type="text" name="sayi2"><br><br>
-  3.Sayıyı Giriniz: <input type="text" name="sayi3"><br><br>
   <input type="submit"  value="Çarp">
   </form>
 <?php }//Form Bitti... ?>
@@ -13,9 +12,8 @@
 if (isset($_GET["sayi1"])){
 $sayi1=$_GET["sayi1"];
 $sayi2=$_GET["sayi2"];
-$sayi3=$_GET["sayi3"];
-$Sonuc=$sayi1*$sayi2*$sayi3;
-echo "<h1>Sonuc: $sayi1*$sayi2*$sayi3=$Sonuc</h1>";
+$Sonuc=$sayi1*$sayi2;
+echo "<h1>Sonuc: $sayi1*$sayi2=$Sonuc</h1>";
 echo "<a href='?'>Başa Dön</a>";
 }
  ?>
